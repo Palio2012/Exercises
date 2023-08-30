e35224ce795c4181925440188d19a6874b437c27
Palio2012 <daniasrayan.com@gmail.com>
2023-08-30 17:14:26 -0300

    How many days you lived.

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double  K, Re, Ra, F;
+
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Please enter the temperature in ºC:");
-        double C = sc.nextDouble();
+        System.out.println("Tell me how old are you, expressed in years, months, and days: ");
 
-        K = C + 273.15;
-        F = C*1.8+32;
-        Re = C*0.8;
-        Ra = C*1.8 + 32 +459.67;
+        int years = sc.nextInt();
+        int months = sc.nextInt();
+        int days = sc.nextInt();
 
-        System.out.println("Fahrenheit: " + F);
-        System.out.println("Kelvin: " + K);
-        System.out.println("Rankine: " + Ra);
-        System.out.println("Réamur: " + Re);
+        int totalDays = (years * 365) + (months * 30) + (days);
 
+        System.out.println("You have lived: " + totalDays + " days." );
     }
 }
